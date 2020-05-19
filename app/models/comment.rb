67cf167476976f171a87cfeee929812ec75e59d2
codecell <ezakaphil@gmail.com>
2020-05-19 21:15:38 +0100

    setup rubocop, stickler linters & Add installation guides to Readme

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  validates :body, presence: true, length: {minimum: 5, maximum: 100}
+  validates :body, presence: true, length: { minimum: 5, maximum: 100 }
   validates :user_id, presence: true
   validates :post_id, presence: true
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: {minimum: 5, maximum: 255}
-  validates :body, presence: true, length: {minimum: 5}
+  validates :title, presence: true, length: { minimum: 5, maximum: 255 }
+  validates :body, presence: true, length: { minimum: 5 }
   validates :user_id, presence: true
 
   belongs_to :user
